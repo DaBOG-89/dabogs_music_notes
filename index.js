79223d5ea068a1047a20bc375cc72d00ba2d12a9
DaBOG 89 <doncrackers45@gmail.com>
2025-10-13 18:47:25 +0100

    js auto resize

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-if ($(window).width() <= 600) {
+function checkWidth() {
+  if (window.innerWidth < 600) {
         $("div.container_1").hide();
 
         $("div.topic_menu > button").show().click(function () {
             $("div.container_1").toggle();    
-         })
+        })
 
         $("h3, p").click(function () {
             $("div.container_1").hide(); 
@@ -12,9 +13,14 @@ if ($(window).width() <= 600) {
         $("div.container_2 > div iframe").hover(function () {
             $("div.container_1").hide();
         })
-}
-else {
-    $("div.container_1").show();
-    $("div.topic_menu > button").hide();  
+  } else {
+        $("div.container_1").show();
+        $("div.topic_menu > button").hide();
+  }
 }
 
+// Run once when page loads
+checkWidth();
+
+// Run again automatically when window is resized
+window.addEventListener('resize', checkWidth);
